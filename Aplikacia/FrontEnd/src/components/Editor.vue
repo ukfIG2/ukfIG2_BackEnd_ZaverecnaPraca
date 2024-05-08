5085b443dfe9e0ceb22118de712420b4ffa24fcb
ukfIG2 <ivan.gabris2@student.ukf.sk>
2024-05-08 20:57:44 +0200

    editor funguje

--- Aplikacia/FrontEnd/src/components/Editor.vue
+++ Aplikacia/FrontEnd/src/components/Editor.vue
@@ -1,22 +1,40 @@
 <template>
-  <div id="editor">
-    <p>This is the editor content.</p>
+  <div>
+    <div id="editor">
+      <p>This is the editor content.</p>
+    </div>
+    <button @click="loadText">Load Text</button>
+    <button @click="showHtml">Show HTML</button>
   </div>
 </template>
 
 <script>
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
+let editorInstance = null;
+
 export default {
   mounted() {
     ClassicEditor
       .create(document.querySelector('#editor'))
       .then(editor => {
-        window.editor = editor;
+        editorInstance = editor;
       })
       .catch(error => {
         console.error('There was a problem initializing the editor.', error);
       });
+  },
+  methods: {
+    loadText() {
+      if (editorInstance) {
+        editorInstance.setData('<p>New text to load.</p>\n<p>Another paragraph.</p>');
+      }
+    },
+    showHtml() {
+      if (editorInstance) {
+        alert(editorInstance.getData());
+      }
+    }
   }
 };
 </script>
